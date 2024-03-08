5aed5d0f3c85a390c8076cfa61392b72c64d97f9
Ivkin-Alexey <ivkin.alexey.sergeevich@gmail.com>
2024-03-08 23:17:30 +0300

    feat: implement groups data fetching

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,4 +1,14 @@
+import {GetGroupsResponse} from "../types";
+
 export function fetchGroups() {
+
+        const response: GetGroupsResponse = {result: 0};
+
         return fetch("groups.json")
             .then(response => response.json())
+            .then(data => {
+                    response.result = 1;
+                    response.data = data;
+                    return response;
+            })
 }
